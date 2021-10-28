78a2bceb22a23f99b71f4da3767d3c2381ba4d9e
Lee Wilson <lee.a.wilson90@gmail.com>
2021-10-28 21:56:58 +0100

    Turn off debug

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,45 @@
+import RateLimiter.State.*
+import java.awt.BorderLayout
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+/**
+ * Example implementation of rate limiter using Java Swing, and log output for reference.
+ */
+fun main() {
+
+    // Implement rate limiter
+    val rateLimiter = object : RateLimiter(
+        timeout = 500,
+        resolution = 200,
+        DEBUG = false,
+    ) {
+        override fun down() {
+            log("DOWN")
+        }
+
+        override fun release() {
+            log("RELEASE")
+        }
+    }
+
+    val frame = JFrame()
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.contentPane.add(JLabel("Window"), BorderLayout.CENTER)
+    val button = JButton("PTT")
+    button.model.addChangeListener {
+        val model = button.model
+        if (model.isArmed) {
+            rateLimiter.submitEvent(DOWN)
+        } else rateLimiter.submitEvent(RELEASE)
+    }
+    frame.contentPane.add(button)
+    button.isVisible = true
+    frame.setSize(200, 200)
+    frame.isVisible = true
 }
+
+var start = System.currentTimeMillis()
+var counter = 0
+fun log(msg: String) = println("${System.currentTimeMillis() - start} | $msg | counter: ${counter++}")
